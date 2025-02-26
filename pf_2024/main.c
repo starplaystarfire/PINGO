3d3e642355b1197262c7d3df96c476b2baa43032
starplaystarfire <127171376+starplaystarfire@users.noreply.github.com>
2025-02-25 20:19:11 -0500

    Update main.c

--- pf_2024/main.c
+++ pf_2024/main.c
@@ -1,8 +1,31 @@
 #include <stdio.h>
+#include <stdio.h>
+#define TAM 50
 
-int main(void)
-{
-    printf("Hello World!\n");
+typedef struct{
+    int idEquipo;
+    char nombreEquipo[TAM];
+    char provincia[TAM];
+    char director[TAM];
+}Equipo;
+
+typedef struct{
+    int idJuego;
+    int idVisitante;
+    int idLocal;
+    int estado;
+    int duracion;
+    int carreras[2][TAM];
+}Juego;
+
+typedef struct{
+    int idSerie;
+    Equipo cantEquipos[20];
+    Juego cantJuegos[100];
+}Serie;
+
+int main(){
+    
     return 0;
 }
 
